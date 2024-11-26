8166c05234c25f99b15d3fa022cd4cfc43fabbd3
SimJeg <sjegou@nvidia.com>
2024-11-26 08:11:48 +0000

    Address PR comment

--- kvpress/presses/tova_press.py
+++ kvpress/presses/tova_press.py
@@ -16,6 +16,8 @@ class TOVAPress(SnapKVPress):
     TOVA (https://arxiv.org/abs/2401.06104) use the attention of the last token averaged across heads
     to estimate the importance of the previous KV pairs. This press was reviewed by Michael Hassid,
     one of the authors of the TOVA paper.
+
+    Official implementation can be found here: https://github.com/schwartz-lab-NLP/TOVA/blob/main/src/tova_cache.py
     """
 
     compression_ratio: float = 0.0
@@ -36,11 +38,13 @@ class TOVAPress(SnapKVPress):
         else:
             attn_weights = self.compute_window_attention(module, hidden_states, keys)
 
-        # Average across heads
+        # Average across heads and repeat num_key_value_head times
         scores = attn_weights.mean(1)
         scores = scores.repeat(1, keys.shape[1], 1)
 
-        # Keep the last token. Very slight difference from TOVA but last attention weight is usually very high
+        # Add back the last token. Use max score to make sure the window is not pruned.
+        # This is a very slight difference from TOVA that don't enforce it, but the
+        # last attention weight is usually very high so it should not change the results.
         scores = F.pad(scores, (0, 1), value=scores.max().item())
 
         return scores
