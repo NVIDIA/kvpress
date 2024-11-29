64c14eef4232a3fe4121b9b8f706c4d218db56a9
maxjeblick <maximilianjeblick@gmail.com>
2024-11-29 16:52:55 +0100

    make test more eplicit

--- tests/wrappers/test_key_rotation_wrapper.py
+++ tests/wrappers/test_key_rotation_wrapper.py
@@ -2,14 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from kvpress import KnormPress
-from kvpress.wrappers.rerotate_keys_wrapper import apply_key_rerotation
+from kvpress import BasePress, KnormPress
+from kvpress.wrappers.rerotate_keys_wrapper import apply_key_rerotation, forward_rerotate_hook
 from tests.fixtures import kv_press_pipeline, unit_test_model  # noqa: F401
 
 
 def test_presses_run(kv_press_pipeline):  # noqa: F811
     press = KnormPress(compression_ratio=0.5)
+    assert not press.forward_hook == forward_rerotate_hook.__get__(press, BasePress)
     wrapped_press = apply_key_rerotation(press)
+    assert press.forward_hook == forward_rerotate_hook.__get__(press, BasePress)
 
     context = "This is a test article. It was written on 2022-01-01."
     questions = ["When was this article written?"]
