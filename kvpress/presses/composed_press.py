f5c98c9247d9c07395be61e0eab6e29fe8bb9091
SimJeg <sjegou@nvidia.com>
2024-12-10 16:19:21 +0000

    Update init

--- kvpress/presses/composed_press.py
+++ kvpress/presses/composed_press.py
@@ -11,10 +11,10 @@ class ComposedPress(BasePress):
     presses: list[BasePress]
 
     def __post_init__(self):
-        self.compression_ratio = 1.0
+        self.compression_ratio = None
 
     def forward_hook(self, module, input, kwargs, output):
-        self.compression_ratio = 1
+        self.compression_ratio = 1.0
         for press in self.presses:
             output = press.forward_hook(module, input, kwargs, output)
             self.compression_ratio *= press.compression_ratio  # type: ignore
