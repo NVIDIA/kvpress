4369c026390a0358d82bb2d56450acfcf889f17a
maxjeblick <maximilianjeblick@gmail.com>
2024-12-10 17:58:04 +0100

    add fixture

--- tests/integration/test_ruler.py
+++ tests/integration/test_ruler.py
@@ -8,6 +8,13 @@ from kvpress import ExpectedAttentionPress, KnormPress, SnapKVPress, StreamingLL
 from tests.fixtures import kv_press_llama3_1_flash_attn_pipeline  # noqa: F401
 
 
+@pytest.fixture(scope="session")
+def df_ruler():
+    df = datasets.load_dataset("simonjegou/ruler", "4096")["test"].to_pandas()
+    df = df.loc[df["task"] == "niah_single_3"].reset_index(drop=True)
+    return df
+
+
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU is not available")
 @pytest.mark.skipif(not is_flash_attn_2_available(), reason="flash_attn is not installed")
 @pytest.mark.parametrize(
@@ -15,11 +22,7 @@ from tests.fixtures import kv_press_llama3_1_flash_attn_pipeline  # noqa: F401
 )
 @pytest.mark.parametrize("compression_ratio", [0.1, 0.2])
 @pytest.mark.parametrize("cache", ["dynamic", "quantized"])
-def test_kv_press_llama3_1_flash_attn_pipeline(
-    kv_press_llama3_1_flash_attn_pipeline, cls, compression_ratio, cache  # noqa: F811
-):
-    df = datasets.load_dataset("simonjegou/ruler", "4096")["test"].to_pandas()
-    df = df.loc[df["task"] == "niah_single_3"].reset_index(drop=True)
+def test_ruler_is_correct(kv_press_llama3_1_flash_attn_pipeline, df_ruler, cls, compression_ratio, cache):  # noqa: F811
     if cls == ThinKPress:
         press = cls(key_channel_compression_ratio=compression_ratio, window_size=2)
     else:
@@ -35,9 +38,9 @@ def test_kv_press_llama3_1_flash_attn_pipeline(
         raise ValueError(f"Unknown cache type: {cache}")
 
     idx = 0
-    context = df.iloc[idx]["context"]
-    question = df.iloc[idx]["question"]
-    true_answer = df.iloc[idx]["answer"][0]
+    context = df_ruler.iloc[idx]["context"]
+    question = df_ruler.iloc[idx]["question"]
+    true_answer = df_ruler.iloc[idx]["answer"][0]
 
     pred_answer = kv_press_llama3_1_flash_attn_pipeline(context, question=question, press=press, cache=cache)["answer"]
     assert true_answer in pred_answer
