d9887ea6a5fd78c6658241ecbbcab3c3c86f34cc
maxjeblick <maximilianjeblick@gmail.com>
2024-12-10 17:37:57 +0100

    import flash attn skip

--- tests/integration/test_quantized_cache.py
+++ tests/integration/test_quantized_cache.py
@@ -2,22 +2,14 @@ import datasets
 import pytest
 import torch
 from transformers import QuantizedCacheConfig, QuantoQuantizedCache
-from transformers.utils import is_optimum_quanto_available
+from transformers.utils import is_flash_attn_2_available, is_optimum_quanto_available
 
 from kvpress import ExpectedAttentionPress
 from tests.fixtures import kv_press_llama3_1_flash_attn_pipeline  # noqa: F401
 
-gpu_available = torch.cuda.is_available()
-try:
-    import flash_attn  # noqa: F401
 
-    flash_attn_installed = True
-except:  # noqa: E722
-    flash_attn_installed = False
-
-
-@pytest.mark.skipif(not gpu_available, reason="GPU is not available")
-@pytest.mark.skipif(not flash_attn_installed, reason="flash_attn is not installed")
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU is not available")
+@pytest.mark.skipif(not is_flash_attn_2_available(), reason="flash_attn is not installed")
 def kv_press_llama3_1_flash_attn_pipeline(kv_press_llama3_1_flash_attn_pipeline):  # noqa: F811
     df = datasets.load_dataset("simonjegou/ruler", "4096")["test"].to_pandas()
     df = df.loc[df["task"] == "niah_single_3"].reset_index(drop=True)
@@ -32,8 +24,8 @@ def kv_press_llama3_1_flash_attn_pipeline(kv_press_llama3_1_flash_attn_pipeline)
     assert true_answer in pred_answer
 
 
-@pytest.mark.skipif(not gpu_available, reason="GPU is not available")
-@pytest.mark.skipif(not flash_attn_installed, reason="flash_attn is not installed")
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU is not available")
+@pytest.mark.skipif(not is_flash_attn_2_available(), reason="flash_attn is not installed")
 @pytest.mark.skipif(not is_optimum_quanto_available(), reason="QuantizedCache is not available")
 def kv_press_llama3_1_flash_attn_pipeline(kv_press_llama3_1_flash_attn_pipeline):  # noqa: F811
     df = datasets.load_dataset("simonjegou/ruler", "4096")["test"].to_pandas()
