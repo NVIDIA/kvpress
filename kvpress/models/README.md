15ad2dbcedb06fea1155dd8c7e533b878040cbab
SimJeg <sjegou@nvidia.com>
2024-12-18 15:45:49 +0000

    Prune useless code

--- kvpress/models/README.md
+++ kvpress/models/README.md
@@ -1,5 +1,3 @@
 ## Registering new attention modules
 
-The `modeling_{name}.py` files have been created running the `utils.rewrite_modeling_scripts` function. This function simply adds the `query_states` in the `cache_kwargs`.
-
-The `utils.update_attn_implementations` function can then bue used to register the attention classes in the `{NAME}_ATTENTION_CLASSES` dictionary.
+The `modeling_{name}.py` files have been created running the `utils.add_query_states_in_kwargs`. The `utils.update_attn_implementations` function can then bue used to register the attention classes in the `{NAME}_ATTENTION_CLASSES` dictionary.
--- kvpress/models/utils.py
+++ kvpress/models/utils.py
@@ -7,41 +7,42 @@ from pathlib import Path
 MODEL_NAMES = ["llama", "mistral", "phi3", "qwen2"]
 
 
-def rewrite_modeling_scripts():
+def add_query_states_in_cache_kwargs():
     """
-    Rewrite the modeling_{name}.py files to include the query_states argument in the cache_kwargs
+    Rewrite the attention modules in modeling_{name} to include the query_states argument in the cache_kwargs
     """
 
+    version = transformers.__version__
     for name in MODEL_NAMES:
         module = importlib.import_module(f"transformers.models.{name}.modeling_{name}")
         pattern = r"cache_kwargs = {(.*?)\}"
         repl = r'cache_kwargs = {\1, "query_states": query_states}'
-        version = transformers.__version__
-        source_code = f"# 🚨🚨🚨 This code has been automatically generated using transformers {version} 🚨🚨🚨\n"
-        source_code += inspect.getsource(module)
-        source_code = re.sub(pattern, repl, source_code)
-        source_code = source_code.replace("from ...", "from transformers.")
-        source_code = source_code.replace("from .", f"from transformers.models.{name}.")
+
+        source_code = ""
+        source_code += f"# 🚨🚨🚨 This code has been automatically generated using transformers {version} 🚨🚨🚨\n\n"
+        source_code += f"from transformers.models.{name}.modeling_{name} import *\n"
+        source_code += "from transformers.modeling_flash_attention_utils import _flash_attention_forward\n\n"
+        attention_classes = getattr(module, f"{name.upper()}_ATTENTION_CLASSES")
+        for cls in attention_classes.values():
+            source_code += re.sub(pattern, repl, inspect.getsource(cls)).strip() + "\n\n"
+
         path = Path(__file__).resolve().parent / f"modeling_{name}.py"
         path.write_text(source_code)
 
 
 def update_attn_implementations():
     """
-    Register the kvpress attention classes in the {NAME}_ATTENTION_CLASSES dictionaries of the transformers models
+    Register the kvpress attention classes in the transformers {NAME}_ATTENTION_CLASSES dictionary
     """
 
     for name in MODEL_NAMES:
         transformers_module = importlib.import_module(f"transformers.models.{name}.modeling_{name}")
-        transformers_attention_classes = getattr(transformers_module, f"{name.upper()}_ATTENTION_CLASSES")
-
         kvpress_module = importlib.import_module(f"kvpress.models.modeling_{name}")
-        kvpress_attention_classes = getattr(kvpress_module, f"{name.upper()}_ATTENTION_CLASSES")
 
-        # Update transformers_attention_classes
-        for key in transformers_attention_classes:
-            transformers_attention_classes[key] = kvpress_attention_classes[key]
+        attention_classes = getattr(transformers_module, f"{name.upper()}_ATTENTION_CLASSES")
+        for key, value in attention_classes.items():
+            attention_classes[key] = getattr(kvpress_module, value.__name__)
 
 
 if __name__ == "__main__":
-    rewrite_modeling_scripts()
+    add_query_states_in_cache_kwargs()
