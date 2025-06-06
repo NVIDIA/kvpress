b377bd0277032a6dce504bae0ea6493a1fa10a62
Kyle Liang <liangmanlai@gmail.com>
2025-06-06 21:26:20 +0800

    update for minor changes and style fixs Signed-off-by: Kyle Liang <liangmanlai@gmail.com>

--- kvpress/presses/lagkv_press.py
+++ kvpress/presses/lagkv_press.py
@@ -12,11 +12,23 @@ from kvpress.presses.scorer_press import ScorerPress
 
 @dataclass
 class LagKVPress(ScorerPress):
-    """Prune KV pairs with lag-relative information (https://arxiv.org/abs/2504.04704)"""
+    """
+    Prune KV pairs with lag-relative information (https://arxiv.org/abs/2504.04704)
+
+    Args:
+        n_sink (`int`):
+            The number of sink tokens.
+        lag_size (`int`):
+            The size of the partition. The subsequent partition will serve as a reference for the prior one.
+        cross_scoring (`bool`):
+            (experimental) if cross scoring is enabled, the score will not be limited to inside partion.
+            Since the score is totally normalized, it's possible use it to allocating KV among heads.
+            This switch will be useful for Press Wrapper like AdaKVPress.
+    """
     n_sink: int = 4
     lag_size: int = 128
-    # if cross scoring is enabled, the score will not be limited to inside partion
     cross_scoring: bool = False
+
     def score(
         self,
         module: nn.Module,
@@ -26,12 +38,67 @@ class LagKVPress(ScorerPress):
         attentions: torch.Tensor,
         kwargs,
     ) -> torch.Tensor:
-        return self._compress_algo(keys, values)
-    
-    def _get_states_score(self, base_len, in_size, end_idx, value):
-        target_v = value[:, :, base_len:end_idx]
-        # partition the key-value
-        target_v = target_v.view(in_size[0], in_size[1], -1, self.lag_size, in_size[-1])
+        base_len = self.n_sink
+        bsz, num_key_value_heads, q_len, d = keys.shape
+        if q_len < base_len + 2 * self.lag_size:
+            # no compression
+            return None
+
+        end_idx = base_len + ((q_len - base_len) // self.lag_size) * self.lag_size
+        tail_len = self.lag_size + q_len - end_idx
+
+        key_score = self._get_states_score(
+            keys[:, :, base_len:end_idx].view(bsz, num_key_value_heads, -1, self.lag_size, d))
+        value_score = self._get_states_score(
+            values[:, :, base_len:end_idx].view(bsz, num_key_value_heads, -1, self.lag_size, d))
+        # score is in range [0, 1]
+        score = (key_score + value_score) / 2
+
+        if not self.cross_scoring:
+            score = score.argsort(dim=-1).argsort(dim=-1) / self.lag_size
+            score = score.to(keys.dtype)
+        # the parts should always keep
+        sink_shape = (bsz, num_key_value_heads, self.n_sink)
+        sink_score = torch.ones(sink_shape, dtype=score.dtype, device=score.device)
+        tail_shape = (bsz, num_key_value_heads, tail_len)
+        tail_score = torch.ones(tail_shape, dtype=score.dtype, device=score.device)
+        score = torch.cat((sink_score, score.reshape(bsz, num_key_value_heads, -1), tail_score), dim=-1)
+        return score
+
+    def compress(
+        self,
+        module: nn.Module,
+        hidden_states: torch.Tensor,
+        keys: torch.Tensor,
+        values: torch.Tensor,
+        attentions: torch.Tensor,
+        kwargs: dict,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Override this to conver the case of short sequence length in self.score"""
+        if self.compression_ratio == 0:
+            return keys, values
+
+        # Compute scores
+        scores = self.score(module, hidden_states, keys, values, attentions, kwargs)
+
+        if scores is None:
+            # no compression
+            return keys, values
+
+        # Get indices of KV pairs with the lowest scores
+        q_len = hidden_states.shape[1]
+        n_kept = int(q_len * (1 - self.compression_ratio))
+        indices = scores.topk(n_kept, dim=-1).indices
+        indices = indices.unsqueeze(-1).expand(-1, -1, -1, module.head_dim)
+
+        # Prune keys and values
+        keys = keys.gather(2, indices).contiguous()
+        values = values.gather(2, indices).contiguous()
+
+        return keys, values
+
+    def _get_states_score(self, target_v):
+        """evaluate the scores of keys and values for each token"""
         ref = target_v[:, :, 1:, :, :]
         v = target_v[:, :, :-1, :, :]
         # lag-relative information
@@ -39,34 +106,4 @@ class LagKVPress(ScorerPress):
         max_r = ref.max(dim=-2).values.unsqueeze(-2).expand(-1, -1, -1, self.lag_size, -1)
 
         score = ((v - min_r) / (max_r - min_r)).std(dim=-1).softmax(dim=-1)
-        
-        return score        
-    
-    def _compress_algo(self, key_states, value_states):
-        base_len = self.n_sink
-        in_size = key_states.shape
-        
-        if in_size[-2] < base_len + 2 * self.lag_size:
-            # no compression
-            return torch.ones(in_size[:-1], dtype=key_states.dtype, device=key_states.device)
-        
-        end_idx = base_len + ((in_size[-2] - base_len) // self.lag_size) * self.lag_size
-        tail_len = self.lag_size + in_size[-2] - end_idx
-        
-        key_score = self._get_states_score(base_len, in_size, end_idx, key_states)
-        value_score = self._get_states_score(base_len, in_size, end_idx, value_states)
-        # score is in range [0, 1]
-        score = (key_score + value_score) / 2
-        
-        if not self.cross_scoring:
-            score = score.argsort(dim=-1).argsort(dim=-1) / self.lag_size
-            score = score.to(key_states.dtype)
-        # the parts should always keep    
-        sink_shape = list(in_size[:-1])
-        sink_shape[-1] = self.n_sink
-        sink_score = torch.ones(sink_shape, dtype=score.dtype, device=score.device)
-        tail_shape = list(in_size[:-1])
-        tail_shape[-1] = tail_len
-        tail_score = torch.ones(tail_shape, dtype=score.dtype, device=score.device)
-        score = torch.cat((sink_score, score.reshape(in_size[0], in_size[1], -1), tail_score), dim=-1)
         return score
