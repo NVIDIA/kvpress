84f82feba822900ea13585b244d312df3273c0bc
GaussonTschen <guoxchen@foxmail.com>
2025-08-06 05:09:37 +0800

    Delete my_debug.sh
    Signed-off-by: GaussonTschen <guoxchen@foxmail.com>

--- my_debug.sh
+++
@@ -1,4 +0,0 @@
-pip install .
-cd evaluation/
-python evaluate.py --press_name sepllm_trnfree --dataset ruler --model meta-llama/Meta-Llama-3.1-8B-Instruct --compression_ratio 0.75 2>&1 | tee tmp.log
-cd -
