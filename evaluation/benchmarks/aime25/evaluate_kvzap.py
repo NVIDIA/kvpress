f216e1c1808a1d370dd185a7b374bb91ea4bcfb3
SimJeg <sjegou@nvidia.com>
2025-12-10 10:00:39 +0000

    Add SDPX
    Signed-off-by: SimJeg <sjegou@nvidia.com>

--- evaluation/benchmarks/aime25/evaluate_kvzap.py
+++ evaluation/benchmarks/aime25/evaluate_kvzap.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 1993-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import uuid
 from tqdm import tqdm
 from pathlib import Path
